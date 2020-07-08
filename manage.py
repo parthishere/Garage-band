4f022611788044371faaa8bc2dae5902631abb5a
jay1704 <67963748+jay1704@users.noreply.github.com>
2020-07-08 18:42:16 +0530

    Add files via upload

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'garage_band.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'linkra.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
